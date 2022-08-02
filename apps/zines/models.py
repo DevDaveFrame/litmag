936f5ed71f9db521a4c9f58cd3e4546135e6bd67
Dave Frame <dframe@industrydive.com>
2022-08-02 16:00:59 -0600

    make zine name required and other model cleanup

--- apps/zines/models.py
+++ apps/zines/models.py
@@ -1,31 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Genre(models.Model):
     name = models.CharField(max_length=60, blank=False, null=True)
     description = models.TextField(blank=True, null=True)
 
-    def __str__(self):
-        return self.name
+    def __str__(self) -> str:
+        return f'{self.name}'
+
 
 class Zine(models.Model):
     genres = models.ManyToManyField('Genre', blank=False)
-    name = models.CharField(max_length=100, blank=False, null=True)
+    name = models.CharField(max_length=100, blank=False, null=False)
     description = models.TextField(blank=False, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.name
+    def __str__(self) -> str:
+        return f'{self.name}'
 
 
 class UserProfile(models.Model):
     class UserRole(models.TextChoices):
-        READER = "Reader"
-        EDITOR = "Editor"
-        AUTHOR = "Author"
-        SUBSCRIBER = "Subscriber"
+        READER = 'Reader'
+        EDITOR = 'Editor'
+        AUTHOR = 'Author'
+        SUBSCRIBER = 'Subscriber'
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
     name = models.CharField(max_length=60, blank=False, null=True)
     role = models.CharField(choices=UserRole.choices, default=UserRole.SUBSCRIBER, max_length=60, blank=False, null=True)
@@ -34,8 +36,9 @@ class UserProfile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self.name} ({self.pronouns})'
+
 
 class Work(models.Model):
     author = models.ForeignKey('UserProfile', blank=False, null=False, on_delete=models.CASCADE)
@@ -47,6 +50,5 @@ class Work(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self.title} by {self.author.name}'
-
