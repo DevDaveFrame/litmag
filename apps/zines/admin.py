2654a0e23cca1bc73caddb0125831f4e9ff3f7cc
Dave Frame <dframe@industrydive.com>
2022-08-02 10:41:05 -0600

    created migration and added models to admin

--- apps/zines/admin.py
+++ apps/zines/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Genre, Zine, UserProfile, Work
 
-# Register your models here.
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Zine)
+class ZineAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Work)
+class WorkAdmin(admin.ModelAdmin):
+    pass
