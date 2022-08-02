2d0e0d7caa36046dc7e86d8ffd11e86b548830e1
Dave Frame <dframe@industrydive.com>
2022-08-02 10:35:53 -0600

    updated user reference

--- apps/zines/models.py
+++ apps/zines/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Genre(models.Model):
     name = models.CharField(max_length=60, blank=False, null=True)
@@ -6,14 +7,14 @@ class Genre(models.Model):
 class Zine(models.Model):
     genres = models.ManyToManyField('Genre', blank=False)
     name = models.CharField(max_length=100, blank=False, null=True)
-    
+
 class UserProfile(models.Model):
     class UserRole(models.TextChoices):
         READER = "Reader"
         EDITOR = "Editor"
         AUTHOR = "Author"
         SUBSCRIBER = "Subscriber"
-    user = models.ForeignKey('User', on_delete=models.CASCADE, blank=False, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
     name = models.CharField(max_length=60, blank=False, null=True)
     role = models.CharField(choices=UserRole.choices, default=UserRole.SUBSCRIBER)
     pronouns = models.CharField(max_length=20, blank=True, null=True)
@@ -24,4 +25,4 @@ class Work(models.Model):
     genres = models.ManyToManyField('Genre')
     abstract = models.TextField(blank=True, null=False)
     body = models.TextField(blank=True, null=False)
-    
+
