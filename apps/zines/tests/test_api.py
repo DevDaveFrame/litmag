e72aa32fffe248b1e88f56cbbf70c19b9ccfd894
Dave Frame <dframe@industrydive.com>
2022-08-03 08:46:07 -0600

    refactored to pytest

--- apps/zines/tests/test_api.py
+++ apps/zines/tests/test_api.py
@@ -1,62 +1,62 @@
-from django.test import TestCase, Client
+import pytest
 from django.urls import reverse
 import json
 
 from apps.zines.models import Zine, Genre, UserProfile
 from django.contrib.auth.models import User
 
-
-class TestZinesAPI(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(
-            username='test_user',
-            password='test_password',
-            email=''
-        )
-        self.client = Client()
-        self.zines_url = reverse('zines-list')
-        self.author = UserProfile.objects.create(
-            user=self.user,
-            name='John Doe',
-            role='Author',
-            pronouns='he/him',
-            birthday='1970-01-01',
-        )
-        self.genre = Genre.objects.create(
-            name='Fantasy',
-            description='A fantasy genre is a genre of literature that typically '
-                        'consists of imaginative, often magical, fantasy stories.',
-        )
+pytestmark = pytest.mark.django_db
+zines_url = reverse('zines-list')
 
 
-class TestGetZines(TestZinesAPI):
-    def test_zero_zines_should_return_empty_list(self) -> None:
-        response = self.client.get(self.zines_url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.loads(response.content), [])
-
-    def test_one_zine_should_return_one_zine(self) -> None:
-        Zine.objects.create(name='New Yonker')
-        response = self.client.get(self.zines_url)
-        response_content = json.loads(response.content)[0]
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response_content.get('name'), 'New Yonker')
+@pytest.fixture
+def sample_author() -> UserProfile:
+    return UserProfile.objects.create(
+        user=User.objects.create(username='test_author'),
+        name='John Doe',
+        role='Author',
+        pronouns='he/him',
+        birthday='1970-01-01',
+    )
 
 
-class TestPostZine(TestZinesAPI):
-    def test_post_zine_should_return_201(self) -> None:
-        response = self.client.post(self.zines_url, {
-            'name': 'New Zine',
-            'description': 'New Zine description',
-            'author': self.author.user.id,
-            'genres': [self.genre.id],
-        })
-        self.assertEqual(response.status_code, 201)
+@pytest.fixture
+def sample_genre() -> Genre:
+    return Genre.objects.create(
+        name='Fantasy',
+        description='A fantasy genre is a genre of literature that typically '
+                    'consists of imaginative, often magical, fantasy stories.',
+    )
 
-    def test_post_zine_should_return_400_if_missing_name(self) -> None:
-        response = self.client.post(self.zines_url, {
-            'description': 'New Zine description',
-            'author': self.author.user.id,
-            'genres': [self.genre.id],
-        })
-        self.assertEqual(response.status_code, 400)
+
+def test_zero_zines_should_return_empty_list(client) -> None:
+    response = client.get(zines_url)
+    assert response.status_code == 200
+    assert json.loads(response.content) == []
+
+
+def test_one_zine_should_return_one_zine(client) -> None:
+    Zine.objects.create(name='New Yonker')
+    response = client.get(zines_url)
+    response_content = json.loads(response.content)[0]
+    assert response.status_code == 200
+    assert response_content.get('name') == 'New Yonker'
+
+
+def test_post_zine_should_return_201(client, sample_author, sample_genre) -> None:
+    response = client.post(zines_url, {
+        'name': 'New Zine',
+        'description': 'New Zine description',
+        'author': sample_author.id,
+        'genres': [sample_genre.id],
+    })
+    assert response.status_code == 201
+
+
+def test_post_zine_should_return_400_if_missing_name(client, sample_author, sample_genre) -> None:
+    response = client.post(zines_url, {
+        'description': 'New Zine description',
+        'author': sample_author.user.id,
+        'genres': [sample_genre.id],
+    })
+    assert response.status_code == 400
